8a1d6a9fbb5c8139e12193bc69fed75ee899b019
Muhammad Athhar Kautsar <athharkautsar14@gmail.com>
2026-09-08 13:46:42 +0700

    chore: bump plugin release version

--- src/ResendPlugin.php
+++ src/ResendPlugin.php
@@ -13,7 +13,7 @@ final class ResendPlugin extends Plugin
         return new PluginManifest(
             id: 'openkos/mail-resend',
             name: 'Resend Mail',
-            version: '0.2.2',
+            version: '0.2.3',
             description: 'Resend mail integration for OpenKOS notifications.',
             coreVersion: '^0.2',
         );
